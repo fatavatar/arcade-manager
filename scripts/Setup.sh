57ea7fd71e4d99c448eadb9e51d27bea97e323b0
Andrew <andrew@thelucks.org>
2016-09-28 23:12:42 -0400

    Adding SSH keys setup

--- scripts/Setup.sh
+++ scripts/Setup.sh
@@ -27,16 +27,29 @@ InstallPkg libswscale-dev
 InstallPkg libavresample-dev
 InstallPkg libfontconfig1-dev
 
-cd ~/develop
-git clone --depth 1 https://github.com/mickelson/sfml-pi sfml-pi
-mkdir sfml-pi/build
-cd sfml-pi/build
-cmake .. -DSFML_RPI=1 -DEGL_INCLUDE_DIR=/opt/vc/include -DEGL_LIBRARY=/opt/vc/lib/libEGL.so -DGLES_INCLUDE_DIR=/opt/vc/include -DGLES_LIBRARY=/opt/vc/lib/libGLESv1_CM.so
-sudo make install
-sudo ldconfig
 
 cd ~/develop
-git clone --depth 1 https://github.com/mickelson/attract attract
-cd attract
-make -j3 USE_GLES=1 
-sudo make install
+if [ ! -d sfml-pi ]; then
+	git clone --depth 1 https://github.com/mickelson/sfml-pi sfml-pi
+	mkdir sfml-pi/build
+	cd sfml-pi/build
+	cmake .. -DSFML_RPI=1 -DEGL_INCLUDE_DIR=/opt/vc/include -DEGL_LIBRARY=/opt/vc/lib/libEGL.so -DGLES_INCLUDE_DIR=/opt/vc/include -DGLES_LIBRARY=/opt/vc/lib/libGLESv1_CM.so
+	sudo make install
+	sudo ldconfig
+fi
+
+cd ~/develop
+if [ ! -d attract ]; then 
+	git clone --depth 1 https://github.com/mickelson/attract attract
+	cd attract
+	make -j3 USE_GLES=1 
+	sudo make install
+fi
+
+#Setup SSH keys
+if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
+	mkdir -p $HOME/.ssh
+	ssh-keygen -t rsa -b 4096 -N "" -f $HOME/.ssh/id_rsa 
+	scp $HOME/.ssh/id_rsa.pub fatavatar@thelucks.org:~/tmp.pub
+	echo "cat ~/tmp.pub >>  ~/.ssh/authorized_keys" | ssh fatavatar@thelucks.org /bin/bash
+fi
