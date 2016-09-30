03f0f866c87898f356feaccd3ad80a7f51288926
Andrew <andrew@thelucks.org>
2016-09-30 11:54:54 -0400

    bugfix

--- scripts/Setup.sh
+++ scripts/Setup.sh
@@ -58,4 +58,4 @@ fi
 
 mkdir -p $HOME/.attract
 rsync -e "/usr/bin/ssh" -av fatavatar@thelucks.org:arcade.thelucks.org/initial_config/ $HOME/.attract/
-cp "$pwd/Refresh Game List.sh" "~/.attract/Attract Mode Setup"
+cp "$pwd/Refresh Game List.sh" "$HOME/.attract/Attract Mode Setup/"
