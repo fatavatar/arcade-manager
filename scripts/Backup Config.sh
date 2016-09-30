886b270325ba2fb7c649759ae9937d9738d3204c
Andrew <andrew@thelucks.org>
2016-09-30 16:39:17 -0400

    bugfix

--- scripts/Backup Config.sh
+++ scripts/Backup Config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -e "/usr/bin/ssh" -av $HOME/.attract fatavatar@arcade.thelucks.org/mirror
-rsync -e "/usr/bin/ssh" -av /opt/retropie/configs fatavatar@arcade.thelucks.org/mirror
-rsync -e "/usr/bin/ssh" -av /opt/retropie/emulators fatavatar@arcade.thelucks.org/mirror
+rsync -e "/usr/bin/ssh" -av $HOME/.attract fatavatar@arcade.thelucks.org:~/mirror
+rsync -e "/usr/bin/ssh" -av /opt/retropie/configs fatavatar@arcade.thelucks.org:~/mirror
+rsync -e "/usr/bin/ssh" -av /opt/retropie/emulators fatavatar@arcade.thelucks.org:~/mirror
